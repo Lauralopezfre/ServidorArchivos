c1239c6fa356353241d0460f1b15740fcc054dbc
Lauralopezfre <15325051520023@cetis108.edu.mx>
2021-02-28 12:02:40 -0700

    Falta el manejo de envio de archivos

--- src/servidor/Servidor.java
+++ src/servidor/Servidor.java
@@ -5,17 +5,36 @@
  */
 package servidor;
 
+import java.io.IOException;
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+
 /**
  *
  * @author laura
  */
 public class Servidor {
-
+    private static final int ECHOMAX = 255;
     /**
      * @param args the command line arguments
      */
-    public static void main(String[] args) {
-        // TODO code application logic here
+    public static void main(String[] args) throws IOException{
+        if(args.length != 1){
+            throw new IllegalArgumentException("Parameter(s): <Port>");
+        }
+        int port = Integer.parseInt(args[0]);
+        
+        DatagramSocket socket = new DatagramSocket(port);
+        DatagramPacket packet = new DatagramPacket(new byte[ECHOMAX], ECHOMAX);
+        
+        while(true){
+            socket.receive(packet);
+            System.out.println("Manejando cliente en: " + packet.getAddress().getHostAddress() + " en el puerto " + packet.getPort());
+            
+            socket.send(packet);
+            packet.setLength(ECHOMAX);
+            
+        }
     }
     
 }
